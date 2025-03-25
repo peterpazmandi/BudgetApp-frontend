df7f10bbc5e2e585bc9e3c0bce021d3630ca4acc
peterpazmandi <peter.pazmandi@outlook.hu>
2025-03-25 23:51:40 +0100

    feat: impl. login ui

--- code/src/components/InputField.tsx
+++ code/src/components/InputField.tsx
@@ -1,24 +1,35 @@
+
 interface InputFieldProps {
+  id: string;
   value: string;
   setValue: (value: string) => void;
   type: string;
   label: string;
+  icon?: React.ReactNode;
 }
 
 const InputField = (props: InputFieldProps) => {
   return (
-    <div className="relative w-100 rounded-full color-input-primary">
+    <div className="relative w-100 rounded-full color-input-primary mt-4 mb-4">
+      {/* Icon */}
+      <div className="absolute w-5 left-3 top-1/2 transform -translate-y-1/2 text-gray-500 dark:invert dark:brightness-0" >
+        {props.icon}
+      </div>
+
+      {/* Input Field */}
       <input
-        id="input-field"
+        id={props.id}
         type={props.type}
         value={props.value}
         onChange={(e) => props.setValue(e.target.value)}
-        className="peer color-input-primary w-100 border border-gray-300 rounded-full px-3 pt-5 pb-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+        className="peer color-input-primary color-text-secondary-invert w-100 border border-gray-300 rounded-full px-10 pt-5 pb-2 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
         placeholder="" /* Keep placeholder empty to prevent overlap */
       />
+
+      {/* Floating Label */}
       <label
-        htmlFor="input-field"
-        className="absolute left-3 top-1 text-gray-500 text-sm transition-all 
+        htmlFor={props.id}
+        className="absolute left-10 top-1 text-gray-500 text-sm transition-all 
               peer-placeholder-shown:top-4 peer-placeholder-shown:text-base 
               peer-placeholder-shown:text-gray-400 
               peer-focus:top-1 peer-focus:text-sm peer-focus:text-blue-500"
--- code/src/pages/auth/components/LoginForm.tsx
+++ code/src/pages/auth/components/LoginForm.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import ButtonInvert from "../../../components/ButtonInvert";
 import H1 from "../../../components/H1";
 import InputField from "../../../components/InputField";
+import { EnvelopeIcon } from "@heroicons/react/24/outline";
+import { LockClosedIcon } from "@heroicons/react/24/outline";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
@@ -9,12 +11,24 @@ const LoginForm = () => {
     <div className="items-center text-center">
       <H1 text="Welcome, back!" className="color-text-primary" />
       <InputField
+        id="email"
         type="text"
         label="Email"
         value={email}
         setValue={function (value: string): void {
           setEmail(value);
         }}
+        icon={<EnvelopeIcon />}
+      />
+      <InputField
+        id="password"
+        type="password"
+        label="Password"
+        value={email}
+        setValue={function (value: string): void {
+          setEmail(value);
+        }}
+        icon={<LockClosedIcon />}
       />
       <ButtonInvert>Login</ButtonInvert>
     </div>
