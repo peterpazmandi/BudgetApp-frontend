44207ee55e656197b4d4b1a3ac75885255cf532c
peterpazmandi <peter.pazmandi@outlook.hu>
2025-07-05 01:11:36 +0200

    fix: handling errors globally

--- code/src/providers/ReactQueryProvider.tsx
+++ code/src/providers/ReactQueryProvider.tsx
@@ -1,12 +1,35 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { useState } from "react";
+import { useNotification } from "../common/hooks/useNotification";
 
 export default function ReactQueryProvider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [queryClient] = useState(() => new QueryClient());
+  const { showError } = useNotification();
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        queryCache: new QueryCache({
+          onError: (error: any) => {
+            const message = (error?.body?.detail as string);
+            showError(message);
+          },
+        }),
+        mutationCache: new MutationCache({
+          onError: (error: any) => {
+            const message = (error?.body?.detail as string);
+            showError(message);
+          },
+        }),
+      })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
